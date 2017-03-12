e6909234f97a42ddda5efe1632b2f1d7fb653664
Kumara <hellboy.ksr2@gmail.com>
2017-03-12 17:02:04 +0530

    Changes to Show the User Name

--- ExpressApp/public/javascripts/chat.js
+++ ExpressApp/public/javascripts/chat.js
@@ -1,4 +1,5 @@
 var socket = io();
+
 function submitfunction(){
   var from = $('#user').val();
   var message = $('#m').val();
@@ -14,33 +15,26 @@ function notifyTyping() {
   socket.emit('notifyUser', user);
 }
 
-socket.on('chatMessage', function(from, msg){
-  var me = $('#user').val();
-  var color = (from == me) ? 'green' : '#009afd';
-  var from = (from == me) ? 'Me' : from;
-  $('#messages').append('<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>');
-});
-
-socket.on('notifyUser', function(user){
-  var me = $('#user').val();
-  if(user != me) {
-    $('#notifyUser').text(user + ' is typing ...');
-  }
-  setTimeout(function(){ $('#notifyUser').text(''); }, 10000);;
-});
-
 $(document).ready(function(){
-  var name = makeid();
-  $('#user').val(name);
-  socket.emit('chatMessage', 'System', '<b>' + name + '</b> has joined the discussion');
+  // On Click on Connect Button Iniitate the User Name and Connect to Server
+  $(document).on('click', '#connectToServer' , function() {
+    $("#userDiv").hide();
+    // Emit Chat message as System when ever the New User Connects.
+    socket.emit('chatMessage', 'System', '<b>' + $('#user').val() + '</b> has joined the discussion');
+
+    socket.on('notifyUser', function(user){
+      var me = $('#user').val();
+      if(user != me) {
+        $('#notifyUser').text(user + ' is typing ...');
+      }
+      setTimeout(function(){ $('#notifyUser').text(''); }, 10000);;
+    });
+
+    socket.on('chatMessage', function(from, msg){
+      var me = $('#user').val();
+      var color = (from == me) ? 'green' : '#009afd';
+      var from = (from == me) ? 'Me' : from;
+      $('#messages').append('<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>');
+    });
+  });
 });
-
-function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for( var i=0; i < 5; i++ ) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
